450885a54f9fa5645f659e7f0ecffd5d85472f99
Dipin Arora <44615649+dipinarora9@users.noreply.github.com>
2021-12-22 13:22:31 +0530

    Update SwiftScreenshotCallbackPlugin.swift

--- ios/Classes/SwiftScreenshotCallbackPlugin.swift
+++ ios/Classes/SwiftScreenshotCallbackPlugin.swift
@@ -5,23 +5,29 @@ public class SwiftScreenshotCallbackPlugin: NSObject, FlutterPlugin {
   static var channel: FlutterMethodChannel?
     
   static var observer: NSObjectProtocol?;
-    
+  static var recordingObserver: NSObjectProtocol?;
+
 
   public static func register(with registrar: FlutterPluginRegistrar) {
     channel  = FlutterMethodChannel(name: "flutter.moum/screenshot_callback", binaryMessenger: registrar.messenger())
     observer = nil;
+    recordingObserver = nil;
     let instance = SwiftScreenshotCallbackPlugin()
     if let channel = channel {
       registrar.addMethodCallDelegate(instance, channel: channel)
     }
-    
   }
 
+
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    if(call.method == "initialize"){
+    if (call.method == "initialize") {
         if(SwiftScreenshotCallbackPlugin.observer != nil) {
             NotificationCenter.default.removeObserver(SwiftScreenshotCallbackPlugin.observer!);
             SwiftScreenshotCallbackPlugin.observer = nil;
+        }
+        if(SwiftScreenshotCallbackPlugin.recordingObserver != nil) {
+            NotificationCenter.default.removeObserver(SwiftScreenshotCallbackPlugin.recordingObserver!);
+            SwiftScreenshotCallbackPlugin.recordingObserver = nil;
         }
         SwiftScreenshotCallbackPlugin.observer = NotificationCenter.default.addObserver(
           forName: UIApplication.userDidTakeScreenshotNotification,
@@ -30,25 +36,50 @@ public class SwiftScreenshotCallbackPlugin: NSObject, FlutterPlugin {
           if let channel = SwiftScreenshotCallbackPlugin.channel {
             channel.invokeMethod("onCallback", arguments: nil)
           }
-
           result("screen shot called")
-      }
+        }
+
+        if #available(iOS 11.0, *) {
+            if UIScreen.main.isCaptured {
+                    print("Screen recording detected then we force the immediate exit of the app!")
+                    if let channel = SwiftScreenshotCallbackPlugin.channel {
+                        channel.invokeMethod("onCallback", arguments: nil)
+                    }
+            }
+            SwiftScreenshotCallbackPlugin.recordingObserver = NotificationCenter.default.addObserver(
+                      forName: UIScreen.capturedDidChangeNotification,
+                      object: nil,
+                      queue: .main) { notification in
+                      if let channel = SwiftScreenshotCallbackPlugin.channel {
+                        channel.invokeMethod("onCallback", arguments: nil)
+                      }
+                result("screen recording turned on")
+            }
+        }
       result("initialize")
-    }else if(call.method == "dispose"){
+    } else if(call.method == "dispose") {
         if(SwiftScreenshotCallbackPlugin.observer != nil) {
             NotificationCenter.default.removeObserver(SwiftScreenshotCallbackPlugin.observer!);
             SwiftScreenshotCallbackPlugin.observer = nil;
         }
+        if(SwiftScreenshotCallbackPlugin.recordingObserver != nil) {
+            NotificationCenter.default.removeObserver(SwiftScreenshotCallbackPlugin.recordingObserver!);
+            SwiftScreenshotCallbackPlugin.recordingObserver = nil;
+        }
         result("dispose")
-    }else{
+    } else {
       result("")
     }
   }
     
-  deinit {
-      if(SwiftScreenshotCallbackPlugin.observer != nil) {
-          NotificationCenter.default.removeObserver(SwiftScreenshotCallbackPlugin.observer!);
-          SwiftScreenshotCallbackPlugin.observer = nil;
-      }
-  }
+    deinit {
+        if(SwiftScreenshotCallbackPlugin.observer != nil) {
+            NotificationCenter.default.removeObserver(SwiftScreenshotCallbackPlugin.observer!);
+            SwiftScreenshotCallbackPlugin.observer = nil;
+        }
+        if(SwiftScreenshotCallbackPlugin.recordingObserver != nil) {
+            NotificationCenter.default.removeObserver(SwiftScreenshotCallbackPlugin.recordingObserver!);
+            SwiftScreenshotCallbackPlugin.recordingObserver = nil;
+        }
+    }
 }
